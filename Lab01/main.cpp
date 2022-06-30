ed74a7aae3c9f0ea825c0f4d2762ca849723cba3
HAGER <48705580+hager51@users.noreply.github.com>
2022-06-30 16:22:48 +0200

    Add files via upload

---
+++ Lab01/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+
+    // Clear Screen :
+    system ("CLS");
+    cout<< "Good-Bye" <<endl;
+
+    // Scape Characters :
+    cout << "new line \n There we go ...!" << endl ;
+    cout << "Space \t there we go ....!" << endl ;
+    cout << "\"Hello World\""<< endl;
+    cout << "\'Hello World\'"<< endl;
+    cout << "Hello world \rhager" << endl;
+    cout << "Hello world \vhager" << endl;
+    cout << "Hello world \fhager" << endl;
+    cout << "Hello world \bhager" << endl;
+
+
+    return 0;
+}
